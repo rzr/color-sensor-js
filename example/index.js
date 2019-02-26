76793041930ef8b6cca6cd6560063897721de2cb
Philippe Coval <p.coval@samsung.com>
2019-02-26 17:53:41 +0100

    WIP: mod: example/index.js (sandbox/rzr/devel/master)
    Change-Id: Ia0924470c461770d16ab6dde72fc1f9e6386cfb9
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/index.js
+++ example/index.js
@@ -38,6 +38,7 @@ var ColorSensor = function (options) {
   this.options = options || {}
   this.options.frequency = this.options.frequency || 1
   this.options.controller = this.options.controller || 'simulator'
+  this.state = 'idle'
 
   return this
 }
